40c873fd5a04472c76cf36b166458afbbf6890c1
egon-isnoise <egonisnoise@proton.me>
2022-08-31 15:29:02 +0200

    made it fully responsive, rects and ellipses too

--- portfolioGrid.js
+++ portfolioGrid.js
@@ -5,16 +5,16 @@ function setup(){
     noiseSeed(69);
     fun = 0;
     inc = 0.05;
+
+    console.log(w);
 }
 
 function draw(){
     background(251);
-    movingFurry(0.035, 20, 150, fun);
+    movingFurry(0.035, 20, w/6, fun);
 
-    if(fun > 1.05 || fun < -0.05) inc *= -1;
+    if(fun > 1.5 || fun < -0.05) inc *= -1;
     fun += inc;
-
-    // console.log(fun);
 }
 
 function movingFurry(inc, scl, pad, fun){
@@ -28,7 +28,7 @@ function movingFurry(inc, scl, pad, fun){
     rows = int(height/this.s);
 
     rotate(-0.1);
-    translate(-this.p/5.7, this.p/2.5);
+    translate(-this.p/7.7, this.p/3.5);
     var yoff = 0;
     //var fun = map(mouseX, 0, width, 0, 3);
     noiseDetail(14, 0.6);
@@ -47,9 +47,9 @@ function movingFurry(inc, scl, pad, fun){
             rotate(v.heading() + this.f);
             lastShifterX = map(x, 0, width, 0, 38);
             lastShifterY = map(y, 0, height, 0, 38);
-            rect(0, 0, this.s +25, 10, 5, 3, 0, 2);
-            ellipse(0, 0, this.s +65, 5);
-            ellipse(lastShifterX, 0, this.s +65, angle *1.3);
+            rect(0, 0, this.s + w/30, w/80, 5, 3, 0, 2);
+            ellipse(0, 0, this.s + w/20, 5);
+            ellipse(lastShifterX, 0, this.s + w/15, angle *1.3);
             
             pop();
         }
