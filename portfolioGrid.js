175cc2dd7acc16eb0ea7bf2ddb71c73cbf7f0774
egon-isnoise <egonisnoise@proton.me>
2022-08-31 10:57:29 +0200

    made it rotate on its own

--- portfolioGrid.js
+++ portfolioGrid.js
@@ -3,17 +3,25 @@ function setup(){
     w = min(windowWidth, windowHeight);
     createCanvas(w,w);
     noiseSeed(69);
+    fun = 0;
+    inc = 0.05;
 }
 
 function draw(){
     background(251);
-    movingFurry(0.035, 20, 150);
+    movingFurry(0.035, 20, 150, fun);
+
+    if(fun > 1.05 || fun < -0.05) inc *= -1;
+    fun += inc;
+
+    // console.log(fun);
 }
 
-function movingFurry(inc, scl, pad){
+function movingFurry(inc, scl, pad, fun){
     this.i = inc;
     this.s = scl;
     this.p =  pad;
+    this.f = fun;
     var cols, rows;
 
     cols = int(width/this.s);
@@ -22,7 +30,7 @@ function movingFurry(inc, scl, pad){
     rotate(-0.1);
     translate(-this.p/5.7, this.p/2.5);
     var yoff = 0;
-    var fun = map(mouseX, 0, width, 0, 3);
+    //var fun = map(mouseX, 0, width, 0, 3);
     noiseDetail(14, 0.6);
 
     for(var y = this.p; y <= height-this.p; y+= this.s){
@@ -36,7 +44,7 @@ function movingFurry(inc, scl, pad){
             stroke(0);
             push();
             translate(x, y);
-            rotate(v.heading() + fun);
+            rotate(v.heading() + this.f);
             lastShifterX = map(x, 0, width, 0, 38);
             lastShifterY = map(y, 0, height, 0, 38);
             rect(0, 0, this.s +25, 10, 5, 3, 0, 2);
