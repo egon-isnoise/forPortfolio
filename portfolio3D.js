f7ba50c5410cf54f3e712066723ee2f122cfe350
egon-isnoise <egonisnoise@proton.me>
2022-09-05 19:42:44 +0200

    resized/tranled fixed everything

--- portfolio3D.js
+++ portfolio3D.js
@@ -17,8 +17,8 @@ function draw(){
     pointLight(225, 225, 255, 0, -200, 0);
     pointLight(255, 250, 225, 0, 1200, 0);
 
-    moverX = map(mouseX, 0, windowWidth, w/6, w-w/6);
-    moverY = map(mouseY, 0, windowHeight, w/6, w-w/6);
+    moverX = map(mouseX, 0, windowWidth, w/4, w-w/4);
+    moverY = map(mouseY, 0, windowHeight, w/4, w-w/4);
 
     translate(moverX - w/2, moverY - w/2);
 
@@ -27,26 +27,26 @@ function draw(){
     fill(34, 76, 53, 200);
     rotateX(millis() / 1380);
     rotateY(millis() / 1180);
-    translate(89, 100, 50);
-    sphere(w/20, 60);
+    translate(105, 120, 50);
+    sphere(w/17, 60);
     rotateX(millis() / 2000);
     rotateY(millis() / 2800);
-    translate(130, 120);
-    sphere(w/12, 70, 120);
+    translate(140, 130);
+    sphere(w/11, 70, 120);
     translate(140, 20, 120);
-    sphere(w/50, 90, 20);
+    sphere(w/45, 90, 20);
     pop();
 
     rotateZ(millis() / 2200);
     rotateX(millis() / 1850);
 
     fill(34, 76, 53, 150);
-    box(w/10, w/10);
+    box(w/8, w/8);
     fill(104, 165, 130, 80);
-    plane(w/4, w/4, 2);
-    rect(w/5, w/5, w/5);
-    rect(-w/5, w/5, w/5);
-    rect(-w/5, (-w/5) *2, w/5);
+    plane(w/3, w/3, 2);
+    rect(w/4, w/4, w/4);
+    rect(-w/4, w/4, w/4);
+    rect(-w/4, (-w/4) *2, w/5);
 }
 
 function windowResized() {
